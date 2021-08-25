438b4ce32d444df4b651d517ccd55481572ce053
Sean <53788990+animite@users.noreply.github.com>
2021-08-25 19:22:30 +0200

    JS files

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", function(){
+    $('.preloader-background').delay(2000).fadeOut('slow');    
+    $('.preloader-wrapper')
+    .delay(2000)   
+    .fadeOut();
+    });
+    
+    var toastHTML = '<p class="zero_PadMargin">Our website does not use cookies but has a Terms of Use. Please read our <a href="html/termsofuse.html">Terms of Use</a> for further details.</p>';
+ M.toast({html: toastHTML , displayLength: 20000});
